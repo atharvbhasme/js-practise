3e5b2bedba44ea772e5cf7704472422c3959936d
Atharv Bhasme <76874157+atharvbhasme@users.noreply.github.com>
2025-09-14 11:22:54 +0530

    Update prototypes.js

--- Objects/prototypes.js
+++ Objects/prototypes.js
@@ -1,7 +1,7 @@
 //what are prototype
 //hidden JS object property
 //it will point to another object which is not exist,
-//inheritance in JS happens through prototypes
+//inheritance in JS happens through prototypes before ES6 classes were introduced
 
 let animal = {
   eats: true,
@@ -43,5 +43,29 @@ class Dog extends Animal {
 
 let d = new Dog("Rex");
 d.speak(); // Rex barks
+
+const person = {
+  name: "Atharv",
+  age: 24,
+  satHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+const emp = {
+  empId: 101,
+  __proto__: person
+}
+
+const citizen = {
+  city: "New York",
+  __proto__: emp
+}
+
+console.log(citizen.name); // Atharv
+console.log(citizen.empId); // 101
+console.log(citizen.city); // New York
+citizen.satHello(); // Hello, my name is Atharv and I am 24 years old.
+
 
 
